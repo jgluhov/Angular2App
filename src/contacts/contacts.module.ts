30a8f747e5023471b8dc22f2d196ab3475bf99f8
JGluhov <evgeniy.glukhov@saprun.com>
2016-09-18 17:08:59 +0300

    add dynamnic route contacts

--- src/contacts/contacts.module.ts
+++ src/contacts/contacts.module.ts
@@ -2,10 +2,11 @@ import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {ContactsComponent} from "./contacts.component";
 import ContactsRoutes from './contacts.routes'
+import {ContactComponent} from "./contact.component";
 
 @NgModule({
     imports: [CommonModule, ContactsRoutes],
-    declarations: [ContactsComponent]
+    declarations: [ContactsComponent, ContactComponent]
 })
 
 export class ContactsModule {}
