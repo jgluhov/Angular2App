89c9a727a5cc44e4d287a86842da769e179c1d50
JGluhov <evgeniy.glukhov@saprun.com>
2016-09-18 17:21:17 +0300

    add routes through ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
     selector: 'app',
     templateUrl: './app.component.html'
 })
 
-export class AppComponent {}
+export class AppComponent {
+    navs = [
+        {url: '', content: 'Home'},
+        {url: 'contacts', content: 'Contacts'},
+        {url: 'contacts/1', content: 'One'}
+    ]
+}
