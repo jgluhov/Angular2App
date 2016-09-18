d1d84ea0745f1e4ef322bf033e29c4145077c578
JGluhov <evgeniy.glukhov@saprun.com>
2016-09-18 13:43:31 +0300

    add contacts component and contacts route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
-import {NgModule, Component} from '@angular/core';
+import {NgModule} from '@angular/core';
 import {BrowserModule}  from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import RouterModule from './app.routes';
 import {HomeComponent} from '../home/home.component';
+import {ContactsComponent} from "../contacts/contacts.component";
 
 @NgModule({
     imports: [BrowserModule, RouterModule],
-    declarations: [AppComponent, HomeComponent],
+    declarations: [AppComponent, HomeComponent, ContactsComponent],
     bootstrap: [AppComponent]
 })
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import {HomeComponent} from '../home/home.component';
+import {ContactsComponent} from '../contacts/contacts.component';
 import {RouterModule} from "@angular/router";
 
 const routes = [
     {
         path: '', component: HomeComponent
+    },
+    {
+        path: 'contacts', component: ContactsComponent
     }
 ];
 
