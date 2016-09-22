e26b63d43798564c3ddc709b59d59c4a70c9f2af
JGluhov <evgeniy.glukhov@saprun.com>
2016-09-22 10:57:16 +0300

    fix app.component.spec and home.component.spec.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,24 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {TestBed, async} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+import {RouterModule} from "@angular/router";
+import appRoutes from './app.routes';
 
-describe('App', () => {
-    beforeEach(() => {
+describe('AppComponent', () => {
+    let fixture: any;
+    let comp: any;
+
+    beforeEach(async(() => {
         TestBed.configureTestingModule({
+            imports: [RouterModule, appRoutes],
             declarations: [AppComponent]
+        })
+        .compileComponents().then(() => {
+            fixture = TestBed.createComponent(AppComponent);
+            comp = fixture.componentInstance;
         });
-    });
+    }));
 
-    it ('should work', () => {
-        let fixture = TestBed.createComponent(AppComponent);
+    it('#create AppComponent', () => {
         expect(fixture.componentInstance instanceof AppComponent).toBe(true, 'should create AppComponent');
     });
 });
--- src/home/home.component.spec.ts
+++ src/home/home.component.spec.ts
@@ -1,15 +1,21 @@
-import {TestBed} from '@angular/core/testing';
+import {TestBed, async} from '@angular/core/testing';
 import {HomeComponent} from './home.component';
 
-describe('Home', () => {
-    beforeEach(() => {
+describe('HomeComponent', () => {
+    let fixture: any;
+    let comp: any;
+
+    beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [HomeComponent]
         })
-    });
+        .compileComponents().then(() => {
+            fixture = TestBed.createComponent(HomeComponent);
+            comp = fixture.componentInstance
+        });
+    }));
 
-    it('should work', () => {
-        let fixture = TestBed.createComponent(HomeComponent);
+    it('#create HomeComponent', () => {
         expect(fixture.componentInstance instanceof HomeComponent).toBe(true, 'should create HomeComponent');
-    })
+    });
 });
