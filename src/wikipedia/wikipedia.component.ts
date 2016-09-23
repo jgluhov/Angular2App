e0c002dd099421d2e66cb606e1a0d7f78dbcfd38
JGluhov <evgeniy.glukhov@saprun.com>
2016-09-23 12:22:36 +0300

    add reactive operators to search into wikipedia component.

--- src/wikipedia/wikipedia.component.ts
+++ src/wikipedia/wikipedia.component.ts
@@ -1,7 +1,9 @@
 import {Component} from '@angular/core';
 import {WikipediaSearchService} from "./wikipedia-search.service";
-
+import {Subject} from 'rxjs/Subject';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
 
 @Component({
     templateUrl: './wikipedia.component.html'
@@ -9,8 +11,14 @@ import 'rxjs/add/operator/map';
 
 export class WikipediaComponent {
     items: Array<string>;
+    term$ = new Subject<string>();
 
-    constructor(private service: WikipediaSearchService) {};
+    constructor(private service: WikipediaSearchService) {
+        this.term$
+            .debounceTime(400)
+            .distinctUntilChanged()
+            .subscribe(term => this.search(term));
+    };
 
     search(term: string) {
         this.service.search(term).subscribe(results => this.items = results);
