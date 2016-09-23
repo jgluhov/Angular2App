085b551d8939f548bc7e7fd319f089714e420962
JGluhov <evgeniy.glukhov@saprun.com>
2016-09-23 12:10:10 +0300

    add wikipedia search form working.

--- src/wikipedia/wikipedia.component.ts
+++ src/wikipedia/wikipedia.component.ts
@@ -1,7 +1,18 @@
 import {Component} from '@angular/core';
+import {WikipediaSearchService} from "./wikipedia-search.service";
+
+import 'rxjs/add/operator/map';
 
 @Component({
     templateUrl: './wikipedia.component.html'
 })
 
-export class WikipediaComponent {}
+export class WikipediaComponent {
+    items: Array<string>;
+
+    constructor(private service: WikipediaSearchService) {};
+
+    search(term: string) {
+        this.service.search(term).subscribe(results => this.items = results);
+    }
+}
--- src/wikipedia/wikipedia.module.ts
+++ src/wikipedia/wikipedia.module.ts
@@ -2,10 +2,13 @@ import {NgModule} from "@angular/core";
 import {WikipediaComponent} from "./wikipedia.component";
 import {CommonModule} from "@angular/common";
 import wikipediaRoutes from './wikipedia.routes';
+import {WikipediaSearchService} from "./wikipedia-search.service";
+import {JsonpModule} from "@angular/http";
 
 @NgModule({
-    imports: [CommonModule, wikipediaRoutes],
-    declarations: [WikipediaComponent]
+    imports: [CommonModule, wikipediaRoutes, JsonpModule],
+    declarations: [WikipediaComponent],
+    providers: [WikipediaSearchService]
 })
 
 export class WikipediaModule {}
