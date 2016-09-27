e2446d3644f13bb4ca304794bfb020c13e658f36
JGluhov <evgeniy.glukhov@saprun.com>
2016-09-27 14:36:56 +0300

    change vendor.

--- src/vendor.ts
+++ src/vendor.ts
@@ -1,16 +1,16 @@
 // Angular 2
 import '@angular/core';
-// import '@angular/common';
-// import '@angular/platform-browser';
+import '@angular/common';
+import '@angular/platform-browser';
 import '@angular/platform-browser-dynamic';
-// import '@angular/http';
+import '@angular/http';
 import '@angular/router';
 
 // RxJS
 // import 'rxjs';
 
 // Lodash
-import 'lodash';
+// import 'lodash';
 
 // Bootstrap
 require('bootstrap/dist/css/bootstrap.css');
