ddea96738bd6779fa79867168a8655258cd1e983
JGluhov <evgeniy.glukhov@saprun.com>
2016-11-03 15:01:31 +0300

    animate navbar.

--- src/navbar/navbar.component.ts
+++ src/navbar/navbar.component.ts
@@ -1,13 +1,37 @@
-import {Component} from '@angular/core';
+import {
+  Component,
+  trigger,
+  style,
+  state,
+  transition,
+  animate
+} from '@angular/core';
 import {Router} from '@angular/router';
 import {AuthService} from '../auth/auth.service';
 
 @Component({
   selector: 'navbar',
+  animations: [
+    trigger('itemState', [
+      state('inactive', style({
+        'color': '#777',
+        'transform': 'scale(1)'
+      })),
+      state('active', style({
+        'color': '#333',
+        'transform': 'scale(1.05)',
+        'font-weight': 600
+      })),
+      transition('* => active', animate('.1s ease-in')),
+      transition('* => inactive', animate('.1s ease-out'))
+    ])
+  ],
   templateUrl: './navbar.component.html'
 })
 
 export class NavbarComponent {
+  itemState: string;
+
   navs = [
     {url: '', content: 'Home'},
     {url: 'posts', content: 'Posts'},
@@ -19,7 +43,12 @@ export class NavbarComponent {
     {url: 'animation', content: 'Animation'}
   ];
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router) {
+  }
+
+  isActiveRoute(route: string) {
+    return (this.router.isActive(route, true)) ? 'active' : 'inactive';
+  }
 
   onSignIn() {
     this.authService.redirectUrl = this.router.routerState.snapshot.url;
