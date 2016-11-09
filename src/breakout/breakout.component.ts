e0db58938eb28e2391a9a28e7802d33e0952fc80
JGluhov <evgeniy.glukhov@saprun.com>
2016-11-09 12:40:39 +0300

    add hit and add code to objects scan.

--- src/breakout/breakout.component.ts
+++ src/breakout/breakout.component.ts
@@ -20,8 +20,40 @@ type TICK = {
   time: number,
   delta: number
 }
+
 type PADDLE_DIRECTION = number;
+
 type PADDLE_POSITION = number;
+
+type BRICK = {
+  x: number,
+  y: number,
+  width: number,
+  height: number
+}
+
+type BALL_POSITION = {
+  x: number,
+  y: number
+}
+
+type BALL_DIRECTION = {
+  x: number,
+  y: number
+}
+
+type BALL = {
+  position: BALL_POSITION,
+  direction: BALL_DIRECTION
+}
+
+type COLLISIONS = {
+  paddle: boolean,
+  floor: boolean,
+  wall: boolean,
+  ceiling: boolean,
+  brick: boolean
+};
 
 @Component({
   templateUrl: './breakout.component.html',
@@ -35,6 +67,13 @@ export class BreakoutComponent implements AfterViewInit {
   PADDLE_WIDTH = 100;
   PADDLE_HEIGHT = 20;
   BALL_SPEED = 60;
+
+  BRICK_GAP = 3;
+  BRICK_HEIGHT = 20;
+  BRICK_COLUMNS = 7;
+  BRICK_ROWS = 5;
+
+  BALL_RADIUS = 10;
 
   @ViewChild('breakoutArea') breakoutArea: ElementRef;
 
@@ -52,12 +91,12 @@ export class BreakoutComponent implements AfterViewInit {
     this.drawTitle();
     this.drawControls();
     this.drawAuthor();
-    this.paddle$.subscribe(x => console.log(x));
+    this.objects$.subscribe(x => console.log(x));
   }
 
   get ticker$(): Observable<TICK> {
 
-    const tick = () => ({ time: Date.now(), delta: 0 });
+    const tick = () => ({time: Date.now(), delta: 0});
 
     return Observable.interval(this.TICKER_INTERVAL, (Scheduler as any).requestAnimationFrame)
       .map(tick)
@@ -111,60 +150,96 @@ export class BreakoutComponent implements AfterViewInit {
       .distinctUntilChanged();
   }
 
-  // factory() {
-  //
-  // }
+  factory() {
+    const countGap = () => this.BRICK_GAP + this.BRICK_GAP * this.BRICK_COLUMNS;
+    let bricks: BRICK[] = [];
 
-  // collision(brick, ball) {
-  //   return ball.position.x + ball.direction.x > brick.x - brick.width / 2
-  //     && ball.position.x + ball.direction.x < brick.x + brick.width / 2
-  //     && ball.position.y + ball.direction.y > brick.y - brick.height / 2
-  //     && ball.position.y + ball.direction.y < brick.y + brick.height / 2;
-  // }
-  //
-  // get objects$() {
-  //   const initialObjects = {
-  //     ball: {
-  //       position: {
-  //         x: this.breakoutArea.nativeElement.width / 2,
-  //         y: this.breakoutArea.nativeElement.height / 2
-  //       },
-  //       direction: {
-  //         x: 2,
-  //         y: 2
-  //       }
-  //     },
-  //     bricks: this.factory(),
-  //     score: 0
-  //   };
-  //
-  //   return this.ticker$.withLatestFrom(this.paddle$)
-  //     .scan(({ball, bricks, collisions}, [ticker, paddle]) => {
-  //
-  //       const survivors = [];
-  //
-  //       collisions = {
-  //         paddle: false,
-  //         floor: false,
-  //         wall: false,
-  //         ceiling: false,
-  //         brick: false
-  //       };
-  //
-  //       ball.position.x = ball.position.x + ball.direction.x * ticker.delta * this.BALL_SPEED;
-  //       ball.position.y = ball.position.y + ball.direction.y * ticker.delta * this.BALL_SPEED;
-  //
-  //       bricks.forEach(brick => {
-  //         if(!collision(brick, ball)) {
-  //           survivors.push(brick);
-  //         } else {
-  //           collisions.brick = true;
-  //           score = score + 10;
-  //         }
-  //       })
-  //
-  //     }, initialObjects);
-  // }
+    const width = (this.breakoutArea.nativeElement.width - countGap()) / this.BRICK_COLUMNS;
+
+    for (let i = 0; i < this.BRICK_ROWS; i++) {
+      for (let j = 0; j < this.BRICK_COLUMNS; j++) {
+        bricks.push({
+          x: j * (width + this.BRICK_GAP) + width / 2 + this.BRICK_GAP,
+          y: i * (this.BRICK_HEIGHT + this.BRICK_GAP) + this.BRICK_HEIGHT / 2 + this.BRICK_GAP + 20,
+          width: width,
+          height: this.BRICK_HEIGHT
+        });
+      }
+    }
+
+    return bricks;
+  }
+
+  collision(brick: BRICK, ball: BALL): boolean {
+    return ball.position.x + ball.direction.x > brick.x - brick.width / 2
+      && ball.position.x + ball.direction.x < brick.x + brick.width / 2
+      && ball.position.y + ball.direction.y > brick.y - brick.height / 2
+      && ball.position.y + ball.direction.y < brick.y + brick.height / 2;
+  }
+
+  hit(paddle: PADDLE_POSITION, ball: BALL): boolean {
+    return ball.position.x > paddle - this.PADDLE_WIDTH / 2
+      && ball.position.x < paddle + this.PADDLE_WIDTH / 2
+      && ball.position.y > this.breakoutArea.nativeElement.height - this.PADDLE_HEIGHT - this.BALL_RADIUS / 2;
+  }
+
+  get objects$() {
+
+    const initialObjects = {
+      ball: {
+        position: {
+          x: this.breakoutArea.nativeElement.width / 2,
+          y: this.breakoutArea.nativeElement.height / 2
+        },
+        direction: {
+          x: 2,
+          y: 2
+        }
+      },
+      bricks: this.factory(),
+      collisions: {
+        paddle: false,
+        floor: false,
+        wall: false,
+        ceiling: false,
+        brick: false
+      },
+      score: 0
+    };
+
+    return this.ticker$.withLatestFrom(this.paddle$)
+      .scan(
+        ({ball, bricks, collisions, score}, [ticker, paddle]) => {
+          console.log(ball, bricks, collisions, score, ticker, paddle);
+          let survivors: BRICK[] = [];
+
+          collisions = {
+            paddle: false,
+            floor: false,
+            wall: false,
+            ceiling: false,
+            brick: false
+          };
+
+          ball.position.x = ball.position.x + ball.direction.x * ticker.delta * this.BALL_SPEED;
+          ball.position.y = ball.position.y + ball.direction.y * ticker.delta * this.BALL_SPEED;
+
+          bricks.forEach((brick: BRICK) => {
+            if (!this.collision(brick, ball)) {
+              survivors.push(brick);
+            } else {
+              collisions.brick = true;
+              score = score + 10;
+            }
+          });
+
+          collisions.paddle = this.hit(paddle, ball);
+
+          return initialObjects;
+        },
+        initialObjects
+      );
+  }
 
   drawTitle() {
     this.context.textAlign = 'center';
