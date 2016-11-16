95d95159613ddf30457c146ed241b5ccc672c562
JGluhov <evgeniy.glukhov@saprun.com>
2016-11-16 11:34:43 +0300

    add datetime to wikipedia search.

--- src/miscellaneous/wikipedia/wikipedia.component.ts
+++ src/miscellaneous/wikipedia/wikipedia.component.ts
@@ -1,20 +1,56 @@
 import {Component, OnInit} from '@angular/core';
 import {WikipediaSearchService} from './wikipedia-search.service';
+
 import {Subject} from 'rxjs/Subject';
-import {Observable} from 'rxjs';
+import 'rxjs/add/operator/timestamp';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/publish';
 
 @Component({
-  templateUrl: './wikipedia.component.html'
+  templateUrl: './wikipedia.component.html',
+  styles: [`
+    time {
+      display: block;
+      height: 30px;
+    }
+  `]
 })
 
 export class WikipediaComponent implements OnInit {
-  items$: Observable<Array<Object>>;
+  time$: Subject<Object>;
+  items$: Subject<Array<Object>>;
   term$ = new Subject<string>();
+
+  dateOptions = {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'
+  };
 
   constructor(private service: WikipediaSearchService) {
   };
 
   ngOnInit() {
-    this.items$ = this.service.search(this.term$);
+    this.time$ = new Subject<Object>();
+    this.items$ = new Subject<Array<Object>>();
+
+    this.service.search(this.term$).subscribe(this.items$);
+
+    this.term$.subscribe(() => this.time$.next({}));
+
+    this.items$
+      .timestamp()
+      .do(() => {
+        console.log('datetime');
+      })
+      .map(item => Object.assign({}, item, {
+        datetime: new Date(item.timestamp).toISOString(),
+        date: new Intl.DateTimeFormat('en-US', this.dateOptions)
+          .format(new Date(item.timestamp))
+      }))
+      .subscribe(this.time$);
   }
 }
