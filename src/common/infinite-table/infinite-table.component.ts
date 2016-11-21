4c7ec356031e4690c6471cb3f3fee9bd4dcd85ea
JGluhov <evgeniy.glukhov@saprun.com>
2016-11-21 12:24:07 +0300

    add initial subject

--- src/common/infinite-table/infinite-table.component.ts
+++ src/common/infinite-table/infinite-table.component.ts
@@ -3,10 +3,13 @@ import {
   ViewChild,
   ElementRef,
   AfterViewInit,
-  Input
+  Input, OnInit
 } from '@angular/core';
 
-import {Observable} from 'rxjs';
+import {
+  Observable,
+  ReplaySubject
+} from 'rxjs';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/startWith';
@@ -15,107 +18,147 @@ import 'rxjs/add/operator/bufferCount';
 @Component({
   selector: 'infinite-table',
   template: `
-    <div #phoneBook>
-      <ul class="list-unstyled" #list></ul>
+    <div class="infinite-table-container" [style.height]="totalContainerHeight" #containerRef>
+      <table class="table table-bordered">
+        <thead #headRef>
+          <tr>
+            <th># Index</th>
+            <th># Value</th>
+          </tr>
+        </thead>
+        <tbody #bodyRef>
+          <tr *ngFor="let item of data; let i = index">
+            <td>{{i + 1}}</td>
+            <td>{{item}}</td>
+          </tr>         
+        </tbody>
+      </table>
     </div>`,
-  styles: [`
-    :host > ul {        
-        height: 300px;
-    }
-  `]
+  styleUrls: ['./infinite-table.component.styl']
 })
 
-export class InfiniteTableComponent implements AfterViewInit {
+export class InfiniteTableComponent implements OnInit, AfterViewInit {
   @Input('data') data: Array<string>;
-  @ViewChild('phoneBook') phoneBook: ElementRef;
 
-  totalResults: number = 10000;
-  rowHeight: number = 30;
+  @ViewChild('containerRef') containerRef: ElementRef;
+  @ViewChild('bodyRef') bodyRef: ElementRef;
+  @ViewChild('headRef') headRef: ElementRef;
+
+  visibleContainerHeight: number = 420;
+  rowTotal: number = 10000;
+
+  headHeight: number = 40;
+  rowHeight: number = 38;
+
   yPosition$: Observable<number>;
-  windowHeight$: Observable<number>;
+  tableHeight$: Observable<number>;
+
+  get totalContainerHeight() {
+    return this.rowHeight * this.rowTotal;
+  }
+
+  get visibleRowsCount() {
+    return Math.ceil((this.visibleContainerHeight - this.headHeight) / this.rowHeight);
+  }
+
+  get bodyScrollTop() {
+    return this.bodyRef.nativeElement.scrollTop;
+  }
+
+  ngOnInit() {
+
+    const initialScrollSubject$ = new ReplaySubject<number>(1);
+    initialScrollSubject$.next(this.bodyScrollTop);
+
+    this.yPosition$ = initialScrollSubject$.merge(
+      Observable.fromEvent(this.bodyRef.nativeElement, 'scroll')
+        .map(() => this.bodyScrollTop)
+    );
+
+    this.yPosition$.subscribe(x => console.log(x));
+  }
 
   ngAfterViewInit() {
-    this.yPosition$ = Observable.fromEvent(window, 'scroll')
-      .map(() => this.phoneBook.nativeElement.scrollY);
 
-    this.windowHeight$ = Observable.fromEvent(window, 'resize')
-      .debounceTime(50)
-      .map(() => this.phoneBook.nativeElement.clientHeight);
-
-    const firstVisibleRow$ = this.yPosition$
-      .map(position => Math.floor(position / this.rowHeight))
-      .distinctUntilChanged()
-      .startWith(0);
-
-    const screenHeight$ = Observable.of(this.phoneBook.nativeElement.clientHeight);
-    const rowCount$ = screenHeight$
-      .map(screenHeight => Math.ceil(screenHeight / this.rowHeight))
-      .distinctUntilChanged();
-
-    const visibleRowIndices$ = Observable.combineLatest(
-      firstVisibleRow$, rowCount$, (firstRow, rowCount) => {
-        const visibleIndices: any = [];
-
-        const lastRow = firstRow + rowCount + 1;
-
-        if (lastRow > this.totalResults) {
-          firstRow -= lastRow - this.totalResults;
-        }
-
-        for (let i = 0; i <= rowCount; i++) {
-          visibleIndices.push(i + firstRow);
-        }
-
-        return visibleIndices;
-      });
-
-
-    const removedIndices$ = visibleRowIndices$
-      .bufferCount(2, 1)
-      .map(buffer => _.difference(_.first(buffer), _.last(buffer)))
-      .filter(difference => !_.isEmpty(difference));
-
-    const addedIndices$ = visibleRowIndices$
-      .bufferCount(2, 1)
-      .map(buffer => _.difference(_.last(buffer), _.first(buffer)))
-      .filter(difference => !_.isEmpty(difference));
-
-    let rows = {};
-
-    const renderRow = (index: number) => {
-      const row = document.createElement('li');
-      row.innerText = index.toString();
-      row.style.top = index * this.rowHeight + 'px';
-      this.phoneBook.nativeElement.appendChild(row);
-      rows[index] = row;
-    };
-
-    const removeRow = (index: number) => {
-      if (!index) {
-        return;
-      }
-
-      rows[index].parentElement.removeChild(rows[index]);
-      rows[index] = null;
-    };
-
-    addedIndices$.subscribe(added => {
-      return _.map(added, renderRow);
-    });
-
-    removedIndices$.subscribe(removed => {
-      return _.map(removed, removeRow);
-    });
-
-    const init = () => {
-      const added: Array<number> = [];
-      for(let i = 0; i < 10; i++) {
-        added.push(i);
-      }
-      _.map(added, renderRow);
-    };
-
-    init();
+    //
+    // this.windowHeight$ = Observable.fromEvent(window, 'resize')
+    //   .debounceTime(50)
+    //   .map(() => this.phoneBook.nativeElement.clientHeight);
+    //
+    // const firstVisibleRow$ = this.yPosition$
+    //   .map(position => Math.floor(position / this.rowHeight))
+    //   .distinctUntilChanged()
+    //   .startWith(0);
+    //
+    // const screenHeight$ = Observable.of(this.phoneBook.nativeElement.clientHeight);
+    // const rowCount$ = screenHeight$
+    //   .map(screenHeight => Math.ceil(screenHeight / this.rowHeight))
+    //   .distinctUntilChanged();
+    //
+    // const visibleRowIndices$ = Observable.combineLatest(
+    //   firstVisibleRow$, rowCount$, (firstRow, rowCount) => {
+    //     const visibleIndices: any = [];
+    //
+    //     const lastRow = firstRow + rowCount + 1;
+    //
+    //     if (lastRow > this.totalResults) {
+    //       firstRow -= lastRow - this.totalResults;
+    //     }
+    //
+    //     for (let i = 0; i <= rowCount; i++) {
+    //       visibleIndices.push(i + firstRow);
+    //     }
+    //
+    //     return visibleIndices;
+    //   });
+    //
+    //
+    // const removedIndices$ = visibleRowIndices$
+    //   .bufferCount(2, 1)
+    //   .map(buffer => _.difference(_.first(buffer), _.last(buffer)))
+    //   .filter(difference => !_.isEmpty(difference));
+    //
+    // const addedIndices$ = visibleRowIndices$
+    //   .bufferCount(2, 1)
+    //   .map(buffer => _.difference(_.last(buffer), _.first(buffer)))
+    //   .filter(difference => !_.isEmpty(difference));
+    //
+    // let rows = {};
+    //
+    // const renderRow = (index: number) => {
+    //   const row = document.createElement('li');
+    //   row.innerText = index.toString();
+    //   row.style.top = index * this.rowHeight + 'px';
+    //   this.phoneBook.nativeElement.appendChild(row);
+    //   rows[index] = row;
+    // };
+    //
+    // const removeRow = (index: number) => {
+    //   if (!index) {
+    //     return;
+    //   }
+    //
+    //   rows[index].parentElement.removeChild(rows[index]);
+    //   rows[index] = null;
+    // };
+    //
+    // addedIndices$.subscribe(added => {
+    //   return _.map(added, renderRow);
+    // });
+    //
+    // removedIndices$.subscribe(removed => {
+    //   return _.map(removed, removeRow);
+    // });
+    //
+    // const init = () => {
+    //   const added: Array<number> = [];
+    //   for(let i = 0; i < 10; i++) {
+    //     added.push(i);
+    //   }
+    //   _.map(added, renderRow);
+    // };
+    //
+    // init();
 
   }
 }
--- src/common/infinite-table/infinite-table.module.ts
+++ src/common/infinite-table/infinite-table.module.ts
@@ -1,7 +1,10 @@
 import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
 import {InfiniteTableComponent} from './infinite-table.component';
 
 @NgModule({
+  imports: [CommonModule],
   declarations: [InfiniteTableComponent],
   exports: [InfiniteTableComponent]
 })
